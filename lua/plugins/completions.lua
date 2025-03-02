8f49945729ac125f32f97dad98854a3322f55d91
Girges Scandar <girges.scandar@getyourguide.com>
2025-03-02 18:47:20 +0100

    move luasnip and nvm-lsp-cmp as deps

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,16 +1,12 @@
 return {
 	{
-		"hrsh7th/cmp-nvim-lsp",
-	},
-	{
-		"L3MON4D3/LuaSnip",
+		"hrsh7th/nvim-cmp",
 		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"L3MON4D3/LuaSnip",
 			"saadparwaiz1/cmp_luasnip",
 			"rafamadriz/friendly-snippets",
 		},
-	},
-	{
-		"hrsh7th/nvim-cmp",
 		config = function()
 			local cmp = require("cmp")
 			require("luasnip.loaders.from_vscode").lazy_load()
