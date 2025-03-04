9e0b8ab019423395873c30473ba7beeb3d4f46bb
Girges Scandar <girges.scandar@getyourguide.com>
2025-03-04 16:04:04 +0100

    add fzf

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,7 +2,13 @@ return {
   {
     "nvim-telescope/telescope.nvim",
     tag = "0.1.8",
-    dependencies = { "nvim-lua/plenary.nvim" },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make",
+      },
+    },
     config = function()
       local builtin = require("telescope.builtin")
       vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
@@ -15,13 +21,31 @@ return {
     "nvim-telescope/telescope-ui-select.nvim",
     config = function()
       require("telescope").setup({
+        pickers = {
+          live_grep = {
+            file_ignore_patterns = { "node_modules/", ".git/", ".venv" },
+            additional_args = function(_)
+              return { "--hidden" }
+            end,
+          },
+          find_files = {
+            file_ignore_patterns = { "node_modules/", ".git/", ".venv" },
+            hidden = true,
+          },
+        },
         extensions = {
+          fzf = {
+            fuzzy = true,
+            override_generic_sorter = true,
+            override_file_sorter = true,
+          },
           ["ui-select"] = {
             require("telescope.themes").get_dropdown({}),
           },
         },
       })
       require("telescope").load_extension("ui-select")
+      require("telescope").load_extension("fzf")
     end,
   },
 }
