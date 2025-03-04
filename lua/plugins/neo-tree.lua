b17b77ba7516449e1c7cb3a283c2544fb50dab41
Girges Scandar <girges.scandar@getyourguide.com>
2025-03-04 13:09:47 +0100

    highlight current file in neo-tree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -8,6 +8,13 @@ return {
     -- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
   },
   config = function()
+    require("neo-tree").setup({
+      buffers = {
+        follow_current_file = {
+          enabled = true,
+        },
+      },
+    })
     vim.keymap.set("n", "<leader>e", ":Neotree filesystem reveal left toggle<CR>")
-  end
+  end,
 }
