5ee4f2310d6b65fc47fdc3fc90585021805a189b
Girges Scandar <girges.scandar@getyourguide.com>
2025-03-07 00:52:58 +0100

    adds snacks removes alpha

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,25 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    dashboard = {
+      -- row = 2,
+      sections = {
+        {
+          section = "terminal",
+          cmd = "img2art media/wave.png --scale 0.1 --threshold 120 --alpha",
+          height = 21,
+          width = 70,
+          indent = -5,
+          padding = 1,
+        },
+        { section = "keys",   padding = 1 },
+        {
+          section = "recent_files",
+          padding = 1,
+          -- padding = { 2, 2 },
+        },
+        { section = "startup" },
+      },
+    },
+  },
+}
