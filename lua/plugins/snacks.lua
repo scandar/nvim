6701a19a1bb2f7aa2009754149fd0edfe78978c4
Girges Scandar <girges.scandar@getyourguide.com>
2025-03-08 23:58:19 +0100

    switch theme to tokyonight

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,26 +1,8 @@
 return {
-  "folke/snacks.nvim",
-  priority = 1000,
-  lazy = false,
-  opts = {
-    dashboard = {
-      -- row = 2,
-      sections = {
-        {
-          section = "terminal",
-          cmd = "img2art ~/.config/nvim/media/wave.png --scale 0.1 --threshold 120 --alpha",
-          height = 21,
-          width = 70,
-          indent = -5,
-          padding = 1,
-        },
-        { section = "keys",   padding = 1 },
-        {
-          section = "recent_files",
-          padding = 1,
-        },
-        { section = "startup" },
-      },
-    },
-  },
+	"folke/snacks.nvim",
+	priority = 1000,
+	lazy = false,
+	opts = {
+		dashboard = {},
+	},
 }
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -3,8 +3,6 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
-vim.g.background = "light"
-
 vim.opt.swapfile = false
 
 -- Navigate vim panes better
@@ -13,6 +11,6 @@ vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
 vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
 vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
 
-vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+-- Hybrid line numbers
 vim.wo.number = true
 vim.wo.relativenumber = true
