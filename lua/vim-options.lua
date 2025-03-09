32ff72a5c353d25b3a06ee4fac6ca749be64ca68
Girges Scandar <girges.scandar@getyourguide.com>
2025-03-09 15:19:36 +0100

    add scrolloff=5

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -4,6 +4,7 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
 vim.opt.swapfile = false
+vim.opt.scrolloff = 5
 
 -- Navigate vim panes better
 vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
