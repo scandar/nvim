48cd194541ef482878abc6e8c42f8c4971261a12
Girges Scandar <girges.scandar@getyourguide.com>
2025-03-13 15:53:44 +0100

    improve vim-test keybinds

--- lua/plugins/vim-test.lua
+++ lua/plugins/vim-test.lua
@@ -4,11 +4,11 @@ return {
 		"preservim/vimux",
 	},
 	config = function()
-		vim.keymap.set("n", "<leader>t", ":TestNearest<CR>")
-		vim.keymap.set("n", "<leader>T", ":TestFile<CR>")
-		vim.keymap.set("n", "<leader>a", ":TestSuite<CR>")
-		vim.keymap.set("n", "<leader>l", ":TestLast<CR>")
-		vim.keymap.set("n", "<leader>g", ":TestVisit<CR>")
+		vim.keymap.set("n", "<leader>tt", ":TestNearest<CR>")
+		vim.keymap.set("n", "<leader>tf", ":TestFile<CR>")
+		vim.keymap.set("n", "<leader>ts", ":TestSuite<CR>")
+		vim.keymap.set("n", "<leader>tl", ":TestLast<CR>")
+		-- vim.keymap.set("n", "<leader>g", ":TestVisit<CR>")
 
 		vim.cmd("let test#strategy='vimux'")
 	end,
