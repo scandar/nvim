fdbf4d9e024e9bba0ceb4a8408c405c1ef89fa76
Girges Scandar <girges.scandar@getyourguide.com>
2025-03-30 19:34:18 +0200

    enable virtual text, undo file, highlight cursor line

--- lua/config.lua
+++ lua/config.lua
@@ -5,6 +5,10 @@ vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
 vim.opt.swapfile = false
 vim.opt.scrolloff = 5
+vim.opt.clipboard = "unnamedplus"
+vim.opt.undofile=true
+vim.opt.ignorecase=true
+vim.opt.cursorline=true
 
 -- Navigate vim panes better
 vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
@@ -16,15 +20,18 @@ vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
 vim.wo.number = true
 vim.wo.relativenumber = true
 
--- Use system clipboard
-vim.opt.clipboard = "unnamedplus"
+-- Enable inline diagnostics/virtual text
+vim.diagnostic.config({ virtual_text = true })
+
+-- <Esc> to exit terminal mode
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
 
 -- Highlight on yank
 vim.api.nvim_create_autocmd("TextYankPost", {
-  group = vim.api.nvim_create_augroup("highlight_yank", {}),
-  desc = "Hightlight selection on yank",
-  pattern = "*",
-  callback = function()
-    vim.highlight.on_yank({ higroup = "IncSearch", timeout = 200 })
-  end,
+	group = vim.api.nvim_create_augroup("highlight_yank", {}),
+	desc = "Hightlight selection on yank",
+	pattern = "*",
+	callback = function()
+		vim.highlight.on_yank({ higroup = "IncSearch", timeout = 200 })
+	end,
 })
