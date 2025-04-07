9c323b3e20e48f51dc1cc6baead321c91e1bca8a
Girges Scandar <girges.scandar@getyourguide.com>
2025-04-07 14:25:39 +0200

    virtual lines

--- lua/config.lua
+++ lua/config.lua
@@ -6,9 +6,9 @@ vim.g.mapleader = " "
 vim.opt.swapfile = false
 vim.opt.scrolloff = 5
 vim.opt.clipboard = "unnamedplus"
-vim.opt.undofile=true
-vim.opt.ignorecase=true
-vim.opt.cursorline=true
+vim.opt.undofile = true
+vim.opt.ignorecase = true
+vim.opt.cursorline = true
 
 -- Navigate vim panes better
 vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
@@ -21,17 +21,17 @@ vim.wo.number = true
 vim.wo.relativenumber = true
 
 -- Enable inline diagnostics/virtual text
-vim.diagnostic.config({ virtual_lines = true })
+vim.diagnostic.config({ virtual_text = true })
 
 -- <Esc> to exit terminal mode
 -- vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
 
 -- Highlight on yank
 vim.api.nvim_create_autocmd("TextYankPost", {
-	group = vim.api.nvim_create_augroup("highlight_yank", {}),
-	desc = "Hightlight selection on yank",
-	pattern = "*",
-	callback = function()
-		vim.highlight.on_yank({ higroup = "IncSearch", timeout = 200 })
-	end,
+  group = vim.api.nvim_create_augroup("highlight_yank", {}),
+  desc = "Hightlight selection on yank",
+  pattern = "*",
+  callback = function()
+    vim.highlight.on_yank({ higroup = "IncSearch", timeout = 200 })
+  end,
 })
