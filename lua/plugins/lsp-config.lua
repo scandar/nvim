7ab1539039b314c9baddb572b9fc34e15b2e4b3f
scandar <scandarg@gmail.com>
2025-08-31 17:32:49 +0200

    simplify and fix vue ls setup

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,15 +1,15 @@
 return {
   {
-    "williamboman/mason.nvim",
+    "mason-org/mason.nvim",
     config = function()
       require("mason").setup()
     end,
   },
   {
-    "williamboman/mason-lspconfig.nvim",
+    "mason-org/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "ts_ls" },
+        ensure_installed = { "lua_ls", "vtsls", "vue_ls" },
       })
     end,
   },
@@ -17,54 +17,39 @@ return {
     "neovim/nvim-lspconfig",
     config = function()
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
-      local lspconfig = require("lspconfig")
-      local vue_language_server = vim.fn.stdpath("data")
+      local vue_language_server_path = vim.fn.stdpath("data")
           .. "/mason/packages/vue-language-server/node_modules/@vue/language-server"
+      local vue_plugin = {
+        name = "@vue/typescript-plugin",
+        location = vue_language_server_path,
+        languages = { "vue" },
+        configNamespace = "typescript",
+      }
 
-      lspconfig.lua_ls.setup({
+      vim.lsp.enable("lua_ls")
+      vim.lsp.config("vtsls", {
         capabilities = capabilities,
-      })
-      lspconfig.rust_analyzer.setup({
-        capabilities = capabilities,
-        on_attach = function(_, bufnr)
-          vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
-        end,
-      })
-      lspconfig.markdown_oxide.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.cssls.setup({
-        capabilities = capabilities,
-        init_options = { provideFormatter = false },
-      })
-      lspconfig.html.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.jsonls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.ts_ls.setup({
-        capabilities = capabilities,
-        init_options = {
-          plugins = {
-            {
-              name = "@vue/typescript-plugin",
-              location = vue_language_server,
-              languages = { "vue" },
+        settings = {
+          vtsls = {
+            tsserver = {
+              globalPlugins = {
+                vue_plugin,
+              },
             },
           },
         },
         filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
       })
-      lspconfig.emmet_language_server.setup({
-        filetypes = { "html", "css", "javascriptreact", "typescriptreact", "vue" },
-        capabilities = capabilities,
-      })
-      lspconfig.stylelint_lsp.setup({
-        capabilities = capabilities,
-        filetypes = { "css", "scss", "less", "html", "vue" },
-      })
+      vim.lsp.enable("vtsls")
+      vim.lsp.enable("vue_ls")
+      vim.lsp.config("cssls", { init_options = { provideFormatter = false } })
+      vim.lsp.enable("cssls")
+      vim.lsp.enable("markdown_oxide")
+      vim.lsp.enable("jsonls")
+      vim.lsp.enable("yamlls")
+      vim.lsp.enable("html")
 
+      -- key bindings
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
       vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
