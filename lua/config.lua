567c1d5722b625057b11c313a6b2c41a5feacf10
Girges Scandar <girges.scandar@getyourguide.com>
2026-01-04 18:39:18 +0100

    better whitespace

--- lua/config.lua
+++ lua/config.lua
@@ -3,7 +3,7 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set list")
-vim.cmd("set lcs+=space:·")
+vim.cmd("set listchars=tab:→\\ ,nbsp:␣,space:•,trail:•,extends:⟩,precedes:⟨")
 vim.g.mapleader = " "
 vim.opt.swapfile = false
 vim.opt.scrolloff = 5
