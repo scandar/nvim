724f1bb5cde1fadcef8513a6f558a8987dec4310
Girges Scandar <girges.scandar@getyourguide.com>
2026-01-04 18:05:18 +0100

    visualize empty spaces

--- lua/config.lua
+++ lua/config.lua
@@ -2,6 +2,8 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+vim.cmd("set list")
+vim.cmd("set lcs+=space:·")
 vim.g.mapleader = " "
 vim.opt.swapfile = false
 vim.opt.scrolloff = 5
